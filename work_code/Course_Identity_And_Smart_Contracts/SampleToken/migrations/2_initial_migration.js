747a7bb34a64ae5ec15301b3164cbb06b909e0c1
Sharon Sheah <sharonsheah@gmail.com>
2021-03-25 22:50:12 +1100

    update project 2

--- work_code/Course_Identity_And_Smart_Contracts/SampleToken/migrations/2_initial_migration.js
+++
@@ -1,5 +0,0 @@
-var SampleToken = artifacts.require("SampleToken");
-
-module.exports = function(deployer) {
-  deployer.deploy(SampleToken, "UdacityExampleToken", "UET", 18, 1000);
-};
--- work_code/Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/constructorContract.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.21;
-
-contract constructorContract {
-    uint public amount;
-
-    constructor (uint value) public {
-        amount = value;
-    }
-}
